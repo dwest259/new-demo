d6dbe7a9196b30a057934bbf5575c7358ab28049
dave <dwest259@gmail.com>
2016-11-22 20:31:49 -0500

    postcss filters

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 
 
@@ -12,7 +16,9 @@ gulp.task('html', function() {
 	});
 	
 gulp.task('styles', function() {
-	console.log("CSS task");
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssvars,nested,autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
 	});
 
 gulp.task('watch', function() {
